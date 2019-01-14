d59b9060b31d2e5537be6bb96ed86abfd560db48
neoashraf <ashrafuddin160@gmail.com>
2019-01-14 19:27:39 +0600

    parsing the request header

--- http-module-prac.js
+++ http-module-prac.js
@@ -2,7 +2,6 @@ const http = require('http');
 const url = require('url');
 // create a http server
 var server = http.createServer((req, res)=>{
-
     // get url and parse it
     var parsedUrl = url.parse(req.url, true);
     // get the pathanme
@@ -13,14 +12,14 @@ var server = http.createServer((req, res)=>{
     var requestMethod = req.method.toLowerCase();
     // get the query string as an object
     var queryStringObject = parsedUrl.query;
+    // get the headers as ana object
+    var reqHeaders = req.headers;
     // send response to the client   
     res.end('Hola Baby!');
     // log message
     console.log(`User is hitting on ${path} with method ${requestMethod} and with query`, queryStringObject);
-
+    console.log(`Requests came with this header`, reqHeaders);
 });
 
 var portNum = 3000;
 server.listen(portNum,() => console.log(`Listening on Port ${portNum}`));
-
-// parse http path
