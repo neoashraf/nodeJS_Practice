3cb6de5ae1d3587e440cec4609aa3aebe10f7504
ashraf_neo <ashraf.udin@selise.ch>
2019-01-14 17:48:41 +0600

    parse the request path

--- http-module-prac.js
+++ http-module-prac.js
@@ -1,9 +1,22 @@
 const http = require('http');
-
+const url = require('url');
 // create a http server
 var server = http.createServer((req, res)=>{
-    console.log('Hola Baby!');
+
+    // get url and parse it
+    var parseUrl = url.parse(req.url, true);
+    // get the pathanme
+    var path = parseUrl.pathname;
+    path = path.replace(/^\/+|\/+$/g, '');
+    // send response to the client
+    res.end('Hola Baby!');
+    // log message
+    console.log(`User is hitting on ${path}`);
+
 });
 
 var portNum = 3000;
 server.listen(portNum,() => console.log(`Listening on Port ${portNum}`));
+
+// parse http path
+
